bf30f5194a1d57c5d3872d02b6e5729a95c31c14
ganjiswati <swati@shrisaitechnology.com>
2023-03-07 15:34:18 +0530

    weekly_timesheet API created

---
+++ api/weekly_timesheet/weekly_timesheet.service.js
@@ -0,0 +1,104 @@
+const pool = require("../../config/db");
+
+module.exports = {
+  create: (data, callback) => {
+    console.log("S Data", data);
+    const sql = `insert into timesheetdb.weekly_timesheet(
+        emp_no,
+        totalhours,
+        hours,
+        project,
+        status,
+        approved_by,
+        reason,
+        remark,
+        overtime,
+        day,
+        date,
+        created_at) 
+        values(?,?,?,?,?,?,?,?,?,?,?,NOW())`;
+    pool.query(
+      sql,
+      [
+        data.emp_no,
+        data.totalhours,
+        data.hours,
+        data.project,
+        data.status,
+        data.approved_by,
+        data.reason,
+        data.remark,
+        data.overtime,
+        data.day,
+        data.date,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          console.log(error);
+          return callback(error);
+        }
+        return callback(null, results);
+      }
+    );
+  },
+  getTimesheet: (callback) => {
+    const sql = `select * from timesheetdb.weekly_timesheet`;
+    pool.query(sql, [], (error, results, fields) => {
+      console.log("sdata=>", results);
+      if (error) {
+        console.log(error);
+        return callback(error);
+      }
+      console.log("result", results);
+      return callback(null, results);
+    });
+  },
+  getTimesheetById: (emp_no, callback) => {
+    const sql = `select * from timesheetdb.weekly_timesheet where emp_no=?`;
+    pool.query(sql, [emp_no], (error, results, fields) => {
+      if (error) {
+        console.log(error);
+        return callback(error);
+      }
+      return callback(null, results[0]);
+    });
+  },
+  updateTimesheet: (data, callback) => {
+    const sql = `update timesheetdb.weekly_timesheet set 
+        totalhours=?,
+        hours=?,
+        project=?,
+        status=?,
+        approved_by=?,
+        reason=?,
+        remark=?,
+        overtime=?,
+        day=?,
+        date=?,
+        updated_at=NOW()
+    where emp_no=?`;
+    pool.query(
+      sql,
+      [
+        data.totalhours,
+        data.hours,
+        data.project,
+        data.status,
+        data.approved_by,
+        data.reason,
+        data.remark,
+        data.overtime,
+        data.day,
+        data.date,
+        data.emp_no,
+      ],
+      (error, results, fields) => {
+        console.log(error, results);
+        if (error) {
+          return callback(error);
+        }
+        return callback(null, results[0]);
+      }
+    );
+  },
+};
--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const weekly_dataRouter = require("./api/weekly_data/weekly_data.router");
 const titlesRouter = require("./api/titles/titles.router");
 const projectsRouter = require("./api/projects/projects.router");
 const holidaysRouter = require("./api/holidays/holidays.router");
+const weekly_timesheetRouter = require("./api/weekly_timesheet/weekly_timesheet.router");
 
 var app = express();
 
@@ -41,6 +42,7 @@ app.use("/api/weekly_data", weekly_dataRouter);
 app.use("/api/titles", titlesRouter);
 app.use("/api/projects", projectsRouter);
 app.use("/api/holidays", holidaysRouter);
+app.use("/api/weekly_timesheet", weekly_timesheetRouter);
 app.listen(process.env.APP_PORT, () => {
   console.log(`server running on port ${process.env.APP_PORT}`);
 });
